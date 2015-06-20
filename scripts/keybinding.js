850f5d0ac7409506f29abd8bcd9d6789e8cb0858
Jake Richan <jake.richan@gmail.com>
2015-06-19 22:32:15 -0600

    add key binding helper script

---
+++ scripts/keybinding.js
@@ -0,0 +1,22 @@
+var bindings = {};
+var KEY_NAMES = {
+  'esc': 27
+};
+
+exports.attach = function (char, callback) {
+  var handler = bindings[char] = keyupHandler.bind(null, char, callback);
+  window.addEventListener('keyup', handler);
+}
+
+exports.removeAll = function (matchChar, callback, evt) {
+  for (var key in bindings) {
+    window.removeEventListener('keyup', bindings[key]);
+  }
+}
+
+function keyupHandler (matchChar, callback, evt) {
+  var keyboardStrings = {'esc': 27};
+  if (evt.target.tagName !== 'INPUT' && evt.target.tagName !== 'TEXTAREA') {
+    if (String.fromCharCode(evt.keyCode) === matchChar.toUpperCase() || evt.keyCode === KEY_NAMES[matchChar]) callback.apply(null);
+  }
+}
