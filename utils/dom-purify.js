d944864007b5f3d2cb9a408d9e3619f76ffb0319
Jake Richan <jake.richan@gmail.com>
2015-09-14 17:04:03 -0600

    fix indentation

--- utils/dom-purify.js
+++ utils/dom-purify.js
@@ -16,14 +16,14 @@ var defaults = {
 };
 
 DOMPurify.addHook('afterSanitizeAttributes', function (node) {
-    // set all elements owning target to target=_blank
-    if ('target' in node) {
-        node.setAttribute('target','_blank');
-    }
-    // set non-HTML/MathML links to xlink:show=new
-    if (!node.hasAttribute('target') && (node.hasAttribute('xlink:href') || node.hasAttribute('href'))){
-        node.setAttribute('xlink:show', 'new');
-    }
+  // set all elements owning target to target=_blank
+  if ('target' in node) {
+    node.setAttribute('target','_blank');
+  }
+  // set non-HTML/MathML links to xlink:show=new
+  if (!node.hasAttribute('target') && (node.hasAttribute('xlink:href') || node.hasAttribute('href'))){
+    node.setAttribute('xlink:show', 'new');
+  }
 });
 
 DOMPurify.addHook('afterSanitizeAttributes', function (node) {
