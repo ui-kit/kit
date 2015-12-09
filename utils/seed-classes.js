a6d952df589c16571ed6014cd39cac93877acce4
Jake Richan <jake.richan@gmail.com>
2015-12-08 17:50:03 -0700

    apply &-className to each base class name it was only applying the postfix to the last class assigned to the element.

--- utils/seed-classes.js
+++ utils/seed-classes.js
@@ -4,8 +4,7 @@ export default function(base, statuses = {}, classes = {}) {
   if (classes[base]) base = classes[base];
 
   var rootStatuses = addStatuses(statuses);
-  var baseRegExp = new RegExp('^' + base + '-?');
-
+  
   return function(className, classStatuses) {
     var allStatuses = classStatuses
       ? addStatuses(classStatuses, rootStatuses)
@@ -13,17 +12,13 @@ export default function(base, statuses = {}, classes = {}) {
 
     if (classes[className]) className = classes[className];
 
-    var baseClassName = typeof className === 'string'
-      ? className.replace('&', base)
-      : base + '';
-
-    if (classes[baseClassName]) baseClassName = classes[baseClassName];
-
-    var statusClasses = allStatuses.length
-      ? baseClassName + allStatuses.join(' ' + baseClassName)
-      : '';
-
-    return `${baseClassName} ${statusClasses}`.trim();
+    return base.split(' ').map(function (b) {
+      var baseClassName = typeof className === 'string' ? className.replace('&', b) : b + '';
+      baseClassName = classes[baseClassName] ? classes[baseClassName] : baseClassName;
+      var statusClasses = allStatuses.length ? baseClassName + allStatuses.join(' ' + baseClassName) : '';
+      
+      return `${baseClassName} ${statusClasses}`.trim();
+    }).join(' ');
   };
 }
 
