93a9ae8d0fbc06c8f2d6d9a894dd92d9873b8b48
Jake Richan <jake.richan@gmail.com>
2016-06-27 17:39:44 -0600

    fixes default inverse sort order, adds options for modifier

--- utils/sort-by.js
+++ utils/sort-by.js
@@ -1,13 +1,16 @@
 exports = module.exports = sortBy;
 exports['default'] = sortBy;
-function sortBy(prop, $get) {
-  var inverse = prop.substr(0, 1) === '!';
+function sortBy(prop, $get, options) {
+  var inverse = prop.substr(0, 1) === '!' || options.inverse;
+  var mod = options.modifier;
   if (inverse) prop = prop.slice(1, prop.length);
   return function(a, b) {
     var aVal = $get(prop, a);
     var bVal = $get(prop, b);
+    aVal = mod && mod(aVal) ? mod(aVal) : aVal;
+    bVal = mod && mod(bVal) ? mod(bVal) : bVal;
     if (aVal === bVal) return 0;
-    var condition = inverse ? (aVal < bVal) : (aVal > bVal);
+    var condition = inverse ? (aVal > bVal) : (aVal < bVal)
     return condition ? -1 : 1;
   }
 }
