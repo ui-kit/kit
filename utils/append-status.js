6d91711aa3a736aa9b89b3704ff74a3196caeade
Jake Richan <jake.richan@gmail.com>
2016-07-25 18:17:24 -0600

    append has- or has-no- when that is the prefix

--- utils/append-status.js
+++ utils/append-status.js
@@ -1,3 +1,7 @@
 export default function(bool, str) {
+  if (str.indexOf('has-') === 0) {
+    var arr = str.split('has-');
+    return arr.join(bool ? 'has-' : 'has-no-');
+  }
   return 'is-' + (bool ? '' : 'not-') + str;
 }
