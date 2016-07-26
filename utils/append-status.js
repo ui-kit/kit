6a8d989e34ecc000f9a21a83b443a18031096e6f
Jake Richan <jake.richan@gmail.com>
2016-07-25 18:27:47 -0600

    use replace for one line return

--- utils/append-status.js
+++ utils/append-status.js
@@ -1,7 +1,4 @@
 export default function(bool, str) {
-  if (str.indexOf('has-') === 0) {
-    var arr = str.split('has-');
-    return arr.join(bool ? 'has-' : 'has-no-');
-  }
+  if (str.indexOf('has-') === 0) return bool ? str : str.replace('has-', 'has-no-');
   return 'is-' + (bool ? '' : 'not-') + str;
 }
